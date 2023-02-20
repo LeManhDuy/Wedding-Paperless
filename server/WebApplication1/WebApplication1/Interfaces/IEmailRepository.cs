76ea9de534ab31e26b712bd3bcf475abcecb97a8
Thong Quang <picothieu@gmail.com>
2023-02-20 11:02:27 +0700

    abc

---
+++ server/WebApplication1/WebApplication1/Interfaces/IEmailRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Interfaces
+{
+    public interface IEmailRepository
+    {
+        string GenerateEmailConfirmToken(Person person);
+        Task ConfirmEmail(string emailToken);
+        Task SendEmail(Message message);
+    }
+}
