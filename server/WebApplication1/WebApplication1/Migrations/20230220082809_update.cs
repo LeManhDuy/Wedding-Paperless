58f4e4f40542d417277fad692a34623f4d68d0c4
tpn <tapnguyen01@gmail.com>
2023-02-21 09:12:53 +0700

    .gitignore is now working

---
+++ server/WebApplication1/WebApplication1/Migrations/20230220082809_update.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "column",
+                table: "Albumns",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "row",
+                table: "Albumns",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "column",
+                table: "Albumns");
+
+            migrationBuilder.DropColumn(
+                name: "row",
+                table: "Albumns");
+        }
+    }
+}
