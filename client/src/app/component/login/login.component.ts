05ca9ee676a59351a205bc19ef970b7dd33d3b57
tpn <tapnguyen01@gmail.com>
2023-02-22 09:29:43 +0700

    [MW-21] Login Dialog

--- client/src/app/component/login/login.component.ts
+++ client/src/app/component/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../../services/login.service";
 import {LoginUser} from "../../models/app-user";
 import { MatDialog } from '@angular/material/dialog';
+import {LoginSuccessDialogComponent} from "../login-success-dialog/login-success-dialog.component";
 
 @Component({
   selector: 'app-login',
@@ -11,18 +12,28 @@ import { MatDialog } from '@angular/material/dialog';
 export class LoginComponent implements OnInit {
   apiData: any = "";
   loginUser: LoginUser = new LoginUser()
-
-  constructor(public loginService: LoginService) {
+  loginSuccess = false;
+  constructor(public loginService: LoginService, private dialog: MatDialog) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.loginSuccess) {
+      const dialogRef = this.dialog.open(LoginSuccessDialogComponent);
+    }
   }
-
+  openLoginSuccessDialog(): void {
+    const dialogRef = this.dialog.open(LoginSuccessDialogComponent, {
+      width: '400px',
+      disableClose: false
+    })
+  }
   login(): void {
     this.loginService.login(this.loginUser)
       .subscribe(response => {
         if (response) {
           console.log(response);
+          this.loginSuccess = true;
+          this.openLoginSuccessDialog();
         } else {
           this.apiData = "*Credential Invalid"
           console.log("Login failed");
