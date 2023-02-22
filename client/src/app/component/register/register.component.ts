7155d14b744ac8d87dd6788aa01dd3ed2b04a091
[Eureka] Duy (Marine) M. LE <lemanhduy311111@gmail.com>
2023-02-22 10:40:14 +0700

    [MW-1] Fix

--- client/src/app/component/register/register.component.ts
+++ client/src/app/component/register/register.component.ts
@@ -1,6 +1,7 @@
 import { RegisterService } from './../../services/register.service';
 import { Component, OnInit } from '@angular/core';
 import { RegisterUser } from 'src/app/models/app-user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,31 @@ import { RegisterUser } from 'src/app/models/app-user';
 export class RegisterComponent implements OnInit {
 
   registerUser: RegisterUser = new RegisterUser()
+  errorMessage: string | undefined
+  //registerForm: FormGroup | undefined;
 
-  constructor(private registerService: RegisterService) { }
+  constructor(private registerService: RegisterService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // this.registerForm = this.formBuilder.group({
+    //   fullname: ['', [Validators.required]],
+    //   username: ['', [Validators.required]],
+    //   email: ['', [Validators.required]],
+    //   password: ['', [Validators.required]],
+    //   confirmpassword: ['', [Validators.required]]
+    // });
   }
 
   register() {
-    this.registerService.register(this.registerUser).subscribe();
+    this.registerService.register(this.registerUser).subscribe(response => {
+      console.log(this.registerUser);
+      if (this.registerUser.password !== this.registerUser.confirmpassword) {
+        this.errorMessage = "Password don't match!";
+      }
+      else {
+        this.errorMessage = "Dont success";
+      }
+    })
   }
 
 }
