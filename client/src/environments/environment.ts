b22c2614715f14a375c144ef7d8fad8ac89e2df5
Lê Mạnh Duy <64477259+LeManhDuy@users.noreply.github.com>
2023-03-13 11:10:14 +0700

    Update environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   // apiURL: 'https://duyle3011.bsite.net/'
     apiURL: 'https://localhost:44328/',
-    apiURLClient: 'https://localhost:4200'
+    apiURLClient: 'https://wedding-paperless.netlify.app'
 };
 
