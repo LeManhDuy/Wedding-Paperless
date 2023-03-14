9657be65591310385bd9d5a5a66af8884bb1b455
tpn <tapnguyen01@gmail.com>
2023-03-14 11:15:37 +0700

    [MW-45] Init

--- client/src/app/components/chart/chart.component.ts
+++ client/src/app/components/chart/chart.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Chart ,ChartDataset, ChartOptions} from "chart.js";
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit{
+  const datasets: ChartDataset[] = [
+    {
+      label: 'Dataset 1',
+      data: [10, 20, 30],
+      fill: 'origin'
+    },
+    {
+      label: 'Dataset 2',
+      data: [40, 50, 60],
+      fill: '+2'
+    },
+  ];
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    const ctx = document.getElementById("myAreaChart") as HTMLCanvasElement;
+    const myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        datasets: this.datasets
+      },
+      options: {
+
+      }
+
+    });
+  }
 }
