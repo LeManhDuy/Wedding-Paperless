ae79db771fb2704f2e80b363dfc738543da7bef7
[Eureka] Duy (Marine) M. LE <lemanhduy311111@gmail.com>
2023-03-14 08:58:44 +0700

    [MW-46] Init

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   // apiURL: 'https://duyle3011.bsite.net/'
-  apiURL: 'http://localhost:44328/',
-   //apiURLClient: 'http://localhost:4200/',
-  apiURLClient: 'https://wedding-paperless.netlify.app/'
+  apiURL: 'https://localhost:44328/',
+  apiURLClient: 'http://localhost:4200/',
+  // apiURLClient: 'https://wedding-paperless.netlify.app/'
 };
 
